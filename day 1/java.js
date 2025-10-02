ec8faec525f9f95ab754aaa471eb099c82dfad86
ansh423 <anirudh159@gmail.com>
2025-10-02 17:39:57 +0530

    Day 1 of  coading

---
+++ day 1/java.js
@@ -0,0 +1,61 @@
+document.querySelector("button").addEventListener("click",function(){
+    document.querySelector("p").innerText="hello world";
+})
+
+
+
+// let input = document.querySelector("input")
+// let p = document.getElementById("output")
+
+// input.addEventListener("input",function(){ 
+//     p.innerText = input.value
+// })
+
+
+
+
+// let btn = document.querySelector("button")
+// let body = document.querySelector("body")
+
+// btn.addEventListener("click",function(){
+//     if(body.style.backgroundColor=="white"){
+//         body.style.backgroundColor="black"
+//     }else{
+//         body.style.backgroundColor="white"
+//     }
+
+// })
+
+
+
+
+// let img  = document.querySelector("img")
+
+// img.addEventListener("mouseover",function(){
+//     img.src="gallery-6.jpg"
+// })
+
+// img.addEventListener("mouseout",function(){
+//     img.src="h1-slider2-280x300.jpg";
+// })
+
+
+
+
+// let btn1 = document.getElementById("btn1");
+// let btn2 = document.getElementById("btn2");
+// let count = 0 ;
+
+// btn1.addEventListener("click",function(){
+//     count++;
+//     document.getElementById("count").innerText=count;
+// })
+
+// btn2.addEventListener("click",function(){
+//     count--;
+//     document.getElementById("count").innerText=count;
+// })
+
+
+
+
